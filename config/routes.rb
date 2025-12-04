633e0fa551245df8e21457adaaf67331671c05af
高田洸輔 <kousukegaotian410@gmail.com>
2025-12-04 15:09:42 +0900

    講座指定のZIPファイルの内容に修正、上書きしました

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api , defaults: { format: :json } do
-     namespace :v1 do
-    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
-    }
-    resources :articles
+  root to: "home#index"
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :articles
     end
-    end
+  end
 end
