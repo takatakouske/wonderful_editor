096cfcab98e3d6efe6aaa500406d26ea87cea784
高田洸輔 <kousukegaotian410@gmail.com>
2025-12-16 18:11:27 +0900

    Postmanを使用するうえでエラーの原因になっていたところを修正しました

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,22 @@
-class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        # ★ CSRFをAPI(JSON)では無効化
+        protect_from_forgery with: :null_session
+        skip_before_action :verify_authenticity_token
+
+        private
+
+        # name を許可（name をバリデーションしている場合）
+        def sign_up_params
+          params.permit(:email, :password, :password_confirmation, :name)
+        end
+
+        def account_update_params
+          params.permit(:email, :password, :password_confirmation, :name)
+        end
+      end
+    end
+  end
 end
