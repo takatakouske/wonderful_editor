e780e7b673b02aa0e70f345f4044bd89b7752a8a
高田洸輔 <kousukegaotian410@gmail.com>
2025-12-17 18:19:55 +0900

    修正した箇所を再度戻す形で修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   namespace :api do
-    namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
-      resources :articles
-    end
+   namespace :v1 do
+    mount_devise_token_auth_for "User", at: "auth",
+      controllers: { registrations: "api/v1/auth/registrations" }
+    resources :articles
   end
 end
