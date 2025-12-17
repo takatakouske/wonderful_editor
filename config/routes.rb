c9f0525cfb5d0a091045524131c8a338dcc2cdc0
高田洸輔 <kousukegaotian410@gmail.com>
2025-12-17 18:26:10 +0900

    end が抜けたまま修正していたのでend を付け足しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   namespace :api do
-   namespace :v1 do
-    mount_devise_token_auth_for "User", at: "auth",
-      controllers: { registrations: "api/v1/auth/registrations" }
-    resources :articles
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth",
+        controllers: {
+          registrations: "api/v1/auth/registrations"
+        }
+
+      resources :articles
+    end
   end
 end
