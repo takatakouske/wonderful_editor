d5a7add6c6a4720c9124e81a433797ce77f80037
高田洸輔 <kousukegaotian410@gmail.com>
2025-12-23 14:24:33 +0900

    登録コントローラでも明示スキップ（保険のため）に修正しました。

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     module Auth
       class RegistrationsController < DeviseTokenAuth::RegistrationsController
-        # ←これでCSRF検証そのものをスキップ（例外にも行かない）
+        # ← ここで CSRF を確実にスキップ
         skip_before_action :verify_authenticity_token, raise: false
         protect_from_forgery with: :null_session
         respond_to :json
