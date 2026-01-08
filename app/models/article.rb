a54a4835372e4d6c5dedfa1c0f76d7bf4e678c06
高田洸輔 <kousukegaotian410@gmail.com>
2026-01-08 14:03:44 +0900

    モデルに enum を定義しました

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes,    dependent: :destroy
 
+  enum status: { draft: 0, published: 1 }, _prefix: true
+
   validates :title, presence: true, length: { maximum: 100 }
   validates :body,  presence: true, length: { maximum: 10_000 }
 end
