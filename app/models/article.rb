8ae5df5d965dbd6a5fe5e2fc19089938de5e41cc
高田洸輔 <kousukegaotian410@gmail.com>
2026-01-11 23:49:30 +0900

    enum 定義を修正、追記しました。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
+# app/models/article.rb
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes,    dependent: :destroy
 
-  enum status: { draft: 0, published: 1 }, _prefix: true
-
   validates :title, presence: true, length: { maximum: 100 }
   validates :body,  presence: true, length: { maximum: 10_000 }
+
+  # enum 定義（デフォルトは draft）
+  enum status: { draft: 0, published: 1 }, _default: :draft
 end
