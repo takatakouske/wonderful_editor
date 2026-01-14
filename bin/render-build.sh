08b8be3389fbcd6c0d8b1f723d8a5644ccd09f74
高田洸輔 <kousukegaotian410@gmail.com>
2026-01-14 22:55:01 +0900

    build スクリプトに yarn を入れました

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Node deps (Webpacker用)
+yarn install --frozen-lockfile || yarn install
+
+# Ruby deps
 bundle install
+
+# アセット & DB
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
