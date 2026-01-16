e9b5617c8d0534b3d3f2bfd2f2a948a8e971df72
高田洸輔 <kousukegaotian410@gmail.com>
2026-01-16 18:07:43 +0900

    フォント用ローダーを定義しました

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,4 +5,18 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
+environment.loaders.append('fonts', {
+  test: /\.(woff2?|eot|ttf|otf|svg)$/i,
+  use: [
+    {
+      loader: 'file-loader',
+      options: {
+        name: '[name]-[hash].[ext]',
+        outputPath: 'fonts/',
+        publicPath: '/packs/fonts/'
+      }
+    }
+  ]
+})
+
 module.exports = environment
