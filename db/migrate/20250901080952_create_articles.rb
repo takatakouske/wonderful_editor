7415f27e2ae86d41bc4685b884dbd35b2b419ee9
高田洸輔 <kousukegaotian410@gmail.com>
2026-01-16 18:22:13 +0900

    クラス名とファイル名を一致させました

--- db/migrate/20250901080952_create_articles.rb
+++ db/migrate/20250901080952_create_articles.rb
@@ -1,11 +1,10 @@
-class CreateComments < ActiveRecord::Migration[6.1]
+class CreateArticles < ActiveRecord::Migration[6.1]
   def change
-    create_table :comments do |t|
-      t.text :body, null: false
-      t.references :user,    null: false, foreign_key: true
-      t.references :article, null: false, foreign_key: true
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :body
+      t.references :user, foreign_key: true
       t.timestamps
     end
-    add_index :comments, [:article_id, :created_at]
   end
 end
